c4eeea0174cce61c9fc0e3391c633d0acff83bce
Prince <vishwakarmaprince833@gmail.com>
2023-11-28 22:40:48 +0530

    fix domain list api

--- routes/domain.js
+++ routes/domain.js
@@ -23,11 +23,10 @@ router.post('/', async (req, res) => {
 })
 
 ///List all registered domain of client
-router.get('/', async (req, res) => {
+router.get('/list', async (req, res) => {
   const clientId = req.client._id;
   const domains = await Domain.find({ client: clientId });
   return res.send(domains);
-
 })
 
 module.exports = router;
